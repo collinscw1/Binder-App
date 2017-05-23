f795d45d8833586cf9b603301f4547fecf0126e0
Chris Collins <collinscw.vt@gmail.com>
2017-05-23 12:59:13 -0400

    Created tag and tagging models to set up OTM relationship with tags and links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,5 @@
 class Link < ApplicationRecord
 	belongs_to :page
+	has_many :taggings
+	has_many :tags, through :taggings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516192853) do
+ActiveRecord::Schema.define(version: 20170523165455) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +23,21 @@ ActiveRecord::Schema.define(version: 20170516192853) do
   end
 
   create_table "pages", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "link_id"
+    t.integer  "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["link_id"], name: "index_taggings_on_link_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
